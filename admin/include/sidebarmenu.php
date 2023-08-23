47e9bdbc4ce14f8ec0c11c1df1fcb83ce9267643
Nitaant Vyas <41328369+Nitaant@users.noreply.github.com>
2023-08-22 22:30:57 -0700

    Add files via upload

---
+++ admin/include/sidebarmenu.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<div class="sidebar-menu">
+  <div class="menu">
+    <ul id="menu" >
+      <li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+      <div class="w3-dropdown-hover">
+        <button class="w3-button"><i class="fa fa-list-ul" aria-hidden="true"></i>Tour Packages
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="w3-dropdown-content w3-bar-block">
+          <a href="create-package.php" class="w3-bar-item w3-button">Create</a>
+          <a href="manage-packages.php" class="w3-bar-item w3-button">Manage</a>
+        </div>
+      </div><br>
+      <a href="manage-users.php" class="w3-bar-item w3-button"><i class="fa fa-users" aria-hidden="true"></i>Manage users</a><br>
+      <a href="manage-bookings.php" class="w3-bar-item w3-button"><i class="fa fa-list" aria-hidden="true"></i>Manage booking</a>
+    </ul>
+  </div>
+</div>
